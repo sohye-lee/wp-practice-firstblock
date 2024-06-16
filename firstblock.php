639863cdd34704e272889cc1306334123c34138c
sohye <sohyek@white64.com>
2024-06-16 11:02:19 -0400

    first

---
+++ firstblock.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: First Block
+ * Plugin URI: https://sohye.dev/
+ * Description: My first block
+ * Author: Sohye Kim
+ * Author URI: https://sohye.dev/
+ */
+
+function blocks_course_firstblock_init() {
+    register_block_type( __DIR__ );
+    
+}
+
+add_action( "init", "blocks_course_firstblock_init" );
